7f74d4a0498c937e91ac54b5af8abdf5bc1b5388
WAM <rushwam@icloud.com>
2021-02-03 16:35:00 +0800

    Update EasyWTEncry.php

--- EasyWTEncry.php
+++ EasyWTEncry.php
@@ -1,5 +1,5 @@
 <?php
-namespace xxxx;
+namespace xxxxx;
 
 /* 字符串加/解密机制 
 ** create ：王智鹏（WAM）
@@ -8,34 +8,89 @@ class EasyWTEncry
 {
 	
 	/* 加密密钥 */
-	private $secret_key = '';
+	private $secret_key;
 
-	private $ipverify = true;
-	
-	public function __construct ($secret_key='', $ipverify=true) {
-		$this->secret_key = $secret_key;
-		$this->ipverify   = $ipverify;
+	/* IP验证 */
+	private $ipverify;
+
+	public function __construct (String $secret_key='', String $ipverify='') 
+	{
+		$this->secret_key  = $secret_key;
+		$this->ipverify    = $ipverify;
 	}
 	
-	/* 加密 */
-	public function encryption ($data, $expire=0, $secret_key='') {
+	/* 获取secrt_key的固定值 */
+	private function secret_solidify_val (String $secret_key) 
+	{
+		
+		$secret_md5     = MD5($secret_key);
+		
+		$secret_md5_len = strlen($secret_md5);
 
-		if (empty($data)) {
+		$secret_crc32   = crc32($secret_md5);
+		
+		$fixed_num = 0;
+
+		for ($i = 0; $i < $secret_md5_len; $i++)
+		{
+			$fixed_num += $secret_crc32 % ord($secret_md5[$i]);
+		}
+
+		$decimal = 2;
+
+		$cardinal_number = $secret_crc32 / $fixed_num / pi();
+
+		$cardinal_number -= floor($cardinal_number);
+
+		$result = 0;
+
+		for ($i = 1; $i <= $decimal; $i++)
+		{
+
+			$cloth = pow(10, $i);
+
+			$result += floor($cardinal_number * $cloth);
+			
+			if ($i == $decimal && $result == 0) {
+				
+				$cardinal_number *= $cloth;
+
+				$i = 1;
+
+			}
+
+		}
+		
+		return $result;
+
+	}
+
+	/* 加密 */
+	public function encryption ($data, Int $expire=0, String $secret_key='') 
+	{
+		
+		if (empty($data)) 
+		{
 			return false;
 		}
 		
 		/* 验证数字 */
-		if (!is_integer($expire)) {
+		if (!is_integer($expire)) 
+		{
 			return false;
-		} else if ($expire > 0) {
+		} 
+		else if ($expire > 0) 
+		{
 			$expire = time()+$expire;
 		}
-
-		$ip = $this->ipverify?$_SERVER['REMOTE_ADDR']:'';
+		
+		$ip = $this->ipverify?:'';
+		
+		$secret_key = !empty($secret_key)?$secret_key:$this->secret_key;
 		
 		$encry_result = base64_encode($this->encryption_operation($data, $ip, $expire, $secret_key));
 		
-		$encry_result_len = strlen($encry_result)/2;
+		$encry_result_len = ceil(strlen($encry_result) / $this->secret_solidify_val($secret_key));
 		
 		$encry_result_substr = substr($encry_result, $encry_result_len).substr($encry_result, 0, $encry_result_len);
 		
@@ -43,9 +98,10 @@ class EasyWTEncry
 	}
 	
 	/* 加密机制 */
-	private function encryption_operation ($data, $ip, $expire, $secret_key) {
+	private function encryption_operation ($data, String $ip, Int $expire, String $secret_key) 
+	{
 		
-		$secret_key_tmp = !empty($secret_key)?$secret_key:$this->secret_key;
+		$secret_key_tmp = $secret_key;
 
 		$json_string = json_encode($data)?:$data;
 		
@@ -58,7 +114,8 @@ class EasyWTEncry
 		
 		$random = ord($secret_key_64[rand(1, $len1-1)]);
 		
-		for ($i = 0; $i < $len2; $i++) {
+		for ($i = 0; $i < $len2; $i++) 
+		{
 			$ord = ord($json_string_64[$i])+$random;
 			$json_string_64[$i] = chr($ord);
 		}
@@ -71,7 +128,8 @@ class EasyWTEncry
 		$random = rand(1, $encry_str_total);
 		
 		$encry_str2 = '';
-		for ($i = 0; $i < $encry_str_total; $i++) {
+		for ($i = 0; $i < $encry_str_total; $i++) 
+		{
 			$ord = ord($encry_str[$i])+$random;
 			$encry_str2 .= chr($ord);
 		}
@@ -82,38 +140,44 @@ class EasyWTEncry
 		$encry_total_len = strlen($encry_total);
 		
 		$encry_total_str = '';
-		for ($i = 0; $i < $encry_total_len; $i++) {
+		for ($i = 0; $i < $encry_total_len; $i++) 
+		{
 			$encry_total_str .= $encry_total[$encry_total_len-($i+1)].$encry_str2[$encry_total[$i]];
 		}
 		$encry_total_str = base64_encode($encry_total_str.chr($encry_str_total+$encry_total));
 		
-		return $encry_total_str.'.'.$encry_str2.'.'.$this->_MD5($encry_total_str.$encry_str2.$expire).'.'.$expire;
+		return $encry_total_str.'.'.$encry_str2.'.'.$this->_MD5($encry_total_str.$this->secret_solidify_val($secret_key_tmp).$encry_str2.$expire).'.'.$expire;
 	}
 	
 	/* 解密 */
-	public function decryption ($string, $secret_key='') {
+	public function decryption (String $string, String $secret_key='') 
+	{
 		
 		$string = strrev($string);
+
+		$secret_key = !empty($secret_key)?$secret_key:$this->secret_key;
 		
-		$string_len = strlen($string)/2;
+		$string_len = strlen($string)-ceil(strlen($string) / $this->secret_solidify_val($secret_key));
 		
 		$encry_result = base64_decode(substr($string, $string_len).substr($string, 0, $string_len));
 		
-		$ip = $this->ipverify?$_SERVER['REMOTE_ADDR']:'';
-
+		$ip = $this->ipverify?:'';
+		
 		return $this->decryption_operation($encry_result, $ip, $secret_key);
 	}
 	
 	/* 解密机制 */
-	private function decryption_operation ($string, $ip, $secret_key) {
+	private function decryption_operation (String $string, String $ip, String $secret_key) 
+	{
 
-		$secret_key_tmp = !empty($secret_key)?$secret_key:$this->secret_key;
+		$secret_key_tmp = $secret_key;
 		
 		$secret_key_64 = $this->_MD5($secret_key_tmp.$ip);
 		
 		$encry_arr = explode('.', $string);
-
-		if (count($encry_arr) !== 4) {
+		
+		if (count($encry_arr) !== 4) 
+		{
 			return false;
 		}
 		
@@ -122,12 +186,14 @@ class EasyWTEncry
 		$md5             = $encry_arr[2];
 		$expire          = $encry_arr[3];
 		
-		if ($expire != 0 && time() > $expire) {
+		/* 验证数据一致性 */
+		if ($this->_MD5($encry_total_str.$this->secret_solidify_val($secret_key_tmp).$encry_str2.$expire) !== $md5) 
+		{
 			return false;
 		}
 		
-		/* 验证数据一致性 */
-		if ($this->_MD5($encry_total_str.$encry_str2.$expire) !== $md5) {
+		if ($expire != 0 && time() > $expire) 
+		{
 			return false;
 		}
 		
@@ -138,8 +204,10 @@ class EasyWTEncry
 		$encry_total_str = substr($encry_total2,0, -1);
 		
 		$encry_total = '';
-		for ($i = 0; $i < strlen($encry_total_str); $i++) {
-			if ($i % 2 == 0) {
+		for ($i = 0; $i < strlen($encry_total_str); $i++) 
+		{
+			if ($i % 2 == 0) 
+			{
 				$encry_total .= $encry_total_str[$i];
 			}
 		}
@@ -153,7 +221,8 @@ class EasyWTEncry
 		$encry_str2_len = strlen($encry_str2);
 		
 		$encry_str = '';
-		for ($i = 0; $i < $encry_str2_len; $i++) {
+		for ($i = 0; $i < $encry_str2_len; $i++) 
+		{
 			$ord = ord($encry_str2[$i])-$random;
 			$encry_str .= chr($ord);
 		}
@@ -170,13 +239,15 @@ class EasyWTEncry
 		
 		$random = ord($sources_data_64[0]) - ord($secret_key_64[$len1/2]);
 		
-		for ($i = 0; $i < $len2; $i++) {
+		for ($i = 0; $i < $len2; $i++) 
+		{
 			$ord = ord($sources_real_data[$i])-$random;
 			$sources_real_data[$i] = chr($ord);
 		}
 		
 		//判断是否与密钥一致
-		if ($secret_key_64 !== $secret_key_64_tmp) {
+		if ($secret_key_64 !== $secret_key_64_tmp) 
+		{
 			return false;
 		}
 		
@@ -186,11 +257,70 @@ class EasyWTEncry
 		
 		return $sources;
 	}
+
+	/* 更新加密字符串有效期 */
+	public function update_expire ($string, String $expire_date) 
+	{
+		
+		$string = strrev($string);
+
+		$secret_key = !empty($secret_key)?$secret_key:$this->secret_key;
+		
+		$string_len = strlen($string)-floor(strlen($string) / $this->secret_solidify_val($secret_key));
+		
+		$encry_result = base64_decode(substr($string, $string_len).substr($string, 0, $string_len));
+		
+		$ip = $this->ipverify?:'';
+		
+		$encry_result = base64_encode($this->update_expire_operator($encry_result, $ip, $secret_key, strtotime($expire_date)));
+
+		$encry_result_len = floor(strlen($encry_result) / $this->secret_solidify_val($secret_key));
+		
+		$encry_result_substr = substr($encry_result, $encry_result_len).substr($encry_result, 0, $encry_result_len);
+		
+		return strrev($encry_result_substr);
+
+	}
+
+	/* 加密字符串验证并更新有效期 */
+	private function update_expire_operator (String $string, String $ip, String $secret_key, Int $new_expire) 
+	{
+
+		$secret_key_64 = $this->_MD5($secret_key.$ip);
+		
+		$encry_arr = explode('.', $string);
+		
+		if (count($encry_arr) !== 4) 
+		{
+			return false;
+		}
+		
+		/* 验证数据一致性 */
+		if ($this->_MD5($encry_arr[0].$this->secret_solidify_val($secret_key).$encry_arr[1].$encry_arr[3]) !== $encry_arr[2]) 
+		{
+			return false;
+		}
+		
+		if ($encry_arr[3] != 0 && time() > $encry_arr[3]) 
+		{
+			return false;
+		}
+
+		$encry_arr[2] = $this->_MD5($encry_arr[0].$this->secret_solidify_val($secret_key).$encry_arr[1].$new_expire);
+		$encry_arr[3] = $new_expire;
+
+		return implode('.', $encry_arr);
+		
+	}
 	
-	private function _MD5 ($val, $flag=true) {
-		if ($flag === true) {
+	private function _MD5 ($val, $flag=true) 
+	{
+		if ($flag === true) 
+		{
 			return substr(MD5($val), 8, 16);
-		} else {
+		} 
+		else 
+		{
 			return MD5($val);
 		}
 	}
